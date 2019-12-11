19f81dbbe155bafbad394f448c7477c14124f575
BlackZan1 <kenshin.nakamura.01@mail.ru>
2019-12-11 15:02:14 +0300

    Updates

---
+++ precache-manifest.15c90de009eda74673f75e38707c6a99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904d4d37425495c7ef61ef9138811812",
+    "url": "/BlackZan1/funBookShop/index.html"
+  },
+  {
+    "revision": "276712c7d137d56bf9ab",
+    "url": "/BlackZan1/funBookShop/static/css/main.0db08d70.chunk.css"
+  },
+  {
+    "revision": "9f0f4be36db73187b845",
+    "url": "/BlackZan1/funBookShop/static/js/2.260a5271.chunk.js"
+  },
+  {
+    "revision": "6019a2db51c90e97b4859c840427a0d6",
+    "url": "/BlackZan1/funBookShop/static/js/2.260a5271.chunk.js.LICENSE"
+  },
+  {
+    "revision": "276712c7d137d56bf9ab",
+    "url": "/BlackZan1/funBookShop/static/js/main.666a50c7.chunk.js"
+  },
+  {
+    "revision": "554866617b2a5bd8ea41",
+    "url": "/BlackZan1/funBookShop/static/js/runtime-main.4a489914.js"
+  },
+  {
+    "revision": "e275bf6b681fd7108a7ddcd22b640097",
+    "url": "/BlackZan1/funBookShop/static/media/loader.e275bf6b.gif"
+  },
+  {
+    "revision": "6bf10cf8b15197ca1225cf18a14ff557",
+    "url": "/BlackZan1/funBookShop/static/media/noCoverBook.6bf10cf8.png"
+  }
+]);
